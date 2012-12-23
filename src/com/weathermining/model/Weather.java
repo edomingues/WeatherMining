f0f0cd0c84417d67749a258054a25a79c4dd69cd
Edgar Domingues <edgar.domingues@gmail.com>
2012-12-23 19:10:00 +0000

    Add ARFF format return data.

--- src/com/weathermining/model/Weather.java
+++ src/com/weathermining/model/Weather.java
@@ -31,11 +31,11 @@ public class Weather {
 
 	public Weather() {}
 	
-	public long getId() { // TODO try private or protected
+	public Long getId() { 
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
--- src/com/weathermining/rest/WeatherDataResource.java
+++ src/com/weathermining/rest/WeatherDataResource.java
@@ -7,22 +7,49 @@ import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.edomingues.gae.persistence.jpa.EntityManagerFactorySingleton;
 import com.weathermining.model.Weather;
+import com.weathermining.weka.WeatherWekaConverterUtils;
 
 @Path("/")
 public class WeatherDataResource {
 
+	private static final Logger LOG = LoggerFactory.getLogger(WeatherDataResource.class);
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Collection<Weather> getData() {
+		Collection<Weather> weatherData = getAllWeatherData();
+		return weatherData;
+	}
+	
+	@GET
+	@Produces("application/arff")
+	public String getArffData() {
+		Collection<Weather> weatherData = getAllWeatherData();		
+		try {
+			return WeatherWekaConverterUtils.convertWeatherDataToArff(weatherData);
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+			throw new WebApplicationException(e, Status.INTERNAL_SERVER_ERROR); 
+//			return "Error: " + e.getMessage();
+		}
+	}
+
+	private Collection<Weather> getAllWeatherData() {
 		EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager();
 		Query q = em.createQuery("SELECT weather FROM " + Weather.class.getName() + " weather");
 		@SuppressWarnings("unchecked")
 		Collection<Weather> weatherData = q.getResultList();
 		return weatherData;
 	}
+	
+	
 }
